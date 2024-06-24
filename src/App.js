f40e359a2036a78e9481d20f00e12f3b62b94c34
rai43 <cheickram@gmailprofessional.com>
2024-06-24 14:58:34 +0000

    In this part, we went through the following concepts: 1. Sorting 2. Filtering 3. Selecting 4. Editing 5. Export

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Table from './Components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
